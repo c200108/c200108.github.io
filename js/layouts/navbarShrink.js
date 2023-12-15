958dc8e6f01450b25281098e60ea4be702e77ba0
c200108 <2547985435@qq.com>
2023-12-15 14:01:14 +0800

    Site updated: 2023-12-15 14:01:12

--- js/layouts/navbarShrink.js
+++ js/layouts/navbarShrink.js
@@ -1,3 +1,5 @@
+import { navigationState } from "../utils.js";
+
 export const navbarShrink = {
   navbarDom: document.querySelector(".navbar-container"),
   leftAsideDom: document.querySelector(".page-aside"),
@@ -6,14 +8,15 @@ export const navbarShrink = {
 
   init() {
     this.navbarHeight = this.navbarDom.getBoundingClientRect().height;
-    this.navbarShrink();
+    this.shrink();
     this.togglenavbarDrawerShow();
+    this.toggleSubmenu();
     window.addEventListener("scroll", () => {
-      this.navbarShrink();
+      this.shrink();
     });
   },
 
-  navbarShrink() {
+  shrink() {
     const scrollTop =
       document.documentElement.scrollTop || document.body.scrollTop;
 
@@ -37,11 +40,7 @@ export const navbarShrink = {
         ...document.querySelectorAll(
           ".navbar-drawer .drawer-navbar-list .drawer-navbar-item",
         ),
-      );
-      domList.push(
-        ...document.querySelectorAll(
-          ".navbar-drawer .drawer-navbar-list .dropdown-item",
-        ),
+        ...document.querySelectorAll(".navbar-drawer .tag-count-item"),
       );
     }
 
@@ -64,6 +63,73 @@ export const navbarShrink = {
       });
     }
   },
+
+  toggleSubmenu() {
+    const toggleElements = document.querySelectorAll("[navbar-data-toggle]");
+
+    toggleElements.forEach((toggle) => {
+      if (!toggle.dataset.eventListenerAdded) {
+        toggle.dataset.eventListenerAdded = "true";
+        toggle.addEventListener("click", function () {
+          // console.log("click");
+          const target = document.querySelector(
+            '[data-target="' + this.getAttribute("navbar-data-toggle") + '"]',
+          );
+          const submenuItems = target.children; // Get submenu items
+          const icon = this.querySelector(".fa-chevron-right");
+
+          if (target) {
+            const isVisible = !target.classList.contains("hidden");
+
+            if (icon) {
+              icon.classList.toggle("icon-rotated", !isVisible);
+            }
+
+            if (isVisible) {
+              // Animate to hide (reverse stagger effect)
+              anime({
+                targets: submenuItems,
+                opacity: 0,
+                translateY: -10,
+                duration: 300,
+                easing: "easeInQuart",
+                delay: anime.stagger(80, { start: 20, direction: "reverse" }),
+                complete: function () {
+                  target.classList.add("hidden");
+                },
+              });
+            } else {
+              // Animate to show with stagger effect
+              target.classList.remove("hidden");
+
+              anime({
+                targets: submenuItems,
+                opacity: [0, 1],
+                translateY: [10, 0],
+                duration: 300,
+                easing: "easeOutQuart",
+                delay: anime.stagger(80, { start: 20 }),
+              });
+            }
+          }
+        });
+      }
+    });
+  },
 };
 
-navbarShrink.init();
+try {
+  swup.hooks.on("page:view", () => {
+    navbarShrink.init();
+    navigationState.isNavigating = false;
+  });
+
+  swup.hooks.on("visit:start", () => {
+    navigationState.isNavigating = true;
+    document.body.classList.remove("navbar-shrink");
+  });
+} catch (error) {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  navbarShrink.init();
+});
--- js/tools/tocToggle.js
+++ js/tools/tocToggle.js
@@ -1,7 +1,9 @@
-/* global function */
+/* main function */
+
+import { main } from "../main.js";
 
 export function initTocToggle() {
-  Global.utils.TocToggle = {
+  const TocToggle = {
     toggleBar: document.querySelector(".page-aside-toggle"),
     postPageContainerDom: document.querySelector(".post-page-container"),
     toggleBarIcon: document.querySelector(".page-aside-toggle i"),
@@ -16,23 +18,23 @@ export function initTocToggle() {
       this.toggleBar &&
         this.toggleBar.addEventListener("click", () => {
           this.isOpenPageAside = !this.isOpenPageAside;
-          Global.styleStatus.isOpenPageAside = this.isOpenPageAside;
-          Global.setStyleStatus();
+          main.styleStatus.isOpenPageAside = this.isOpenPageAside;
+          main.setStyleStatus();
           this.changePageLayoutWhenOpenToggle(this.isOpenPageAside);
         });
     },
 
+    toggleClassName(element, className, condition) {
+      if (element) {
+        element.classList.toggle(className, condition);
+      }
+    },
     changePageLayoutWhenOpenToggle(isOpen) {
-      this.toggleBarIcon &&
-        (this.toggleBarIcon.className = isOpen
-          ? "fas fa-indent"
-          : "fas fa-outdent");
-      this.postPageContainerDom.className = isOpen
-        ? "post-page-container show-toc"
-        : "post-page-container";
-      this.mainContentDom.className = isOpen
-        ? "main-content has-toc"
-        : "main-content";
+      this.toggleClassName(this.toggleBarIcon, "fas", isOpen);
+      this.toggleClassName(this.toggleBarIcon, "fa-indent", isOpen);
+      this.toggleClassName(this.toggleBarIcon, "fa-outdent", !isOpen);
+      this.toggleClassName(this.postPageContainerDom, "show-toc", isOpen);
+      this.toggleClassName(this.mainContentDom, "has-toc", isOpen);
     },
 
     pageAsideHandleOfTOC(isOpen) {
@@ -41,9 +43,12 @@ export function initTocToggle() {
       this.changePageLayoutWhenOpenToggle(isOpen);
     },
   };
-  Global.utils.TocToggle.initToggleBarButton();
+
+  TocToggle.initToggleBarButton();
+  return TocToggle;
 }
 
+// Event listeners
 try {
   swup.hooks.on("page:view", () => {
     initTocToggle();
